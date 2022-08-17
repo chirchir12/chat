05bc2f6198097d7bad4f5951d5501b0beb7d0b66
chirchir <chirchir7370@gmail.com>
2022-08-17 11:35:04 +0300

    project set up - ws configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux'
+import React, { Component } from "react"
+import Chat from './components/Chat';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const socket = new WebSocket('ws://localhost:8989')
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount(props){
+    socket.onopen = () => {
+      console.log('tls connection established')
+      socket.send(JSON.stringify({connection:true}))
+    }
+    
+  }
+  render(){
+    const {auth} = this.props
+    console.log(auth, 'as auth')
+
+    return (
+      <>
+      {auth && auth.username ? <Chat socket ={socket}/> : <Login  socket ={socket}/>}
+      </>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state)=> {
+  console.log(state, 'new state')
+  return {
+    auth:state.auth
+  }
+}
+
+
+export default connect(mapStateToProps,null)(App);
